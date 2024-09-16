7e87cd0914a757c23f6cd203058d024be3c9f610
Noriyuki Fujimura <nori_fujimura@mac.com>
2024-09-17 02:44:22 +0900

    update

--- python/light-test.py
+++ python/light-test.py
@@ -11,6 +11,8 @@ import json
 from ctypes import *
 from bleak import BleakClient
 
+import colorsys
+
 #address = "19C40D9B-F748-1109-CF66-67D6BB739283" # 通信先のMacアドレス
 address = "24:58:7C:5C:83:DD"
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8" # CHARACTERISTIC_UUID
@@ -18,6 +20,13 @@ CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8" # CHARACTERISTIC_UU
 
 x = 0
 y = 0
+hue = 0
+lightness = 0
+saturation = 0
+r = 0
+g = 0
+b = 0
+
 
 async def say_after(delay, what):
     print(f"prepare {what} at {time.strftime('%X')}")
@@ -47,6 +56,42 @@ async def ble_byte_reconnect(address,delay):
                 message = bytearray(2)
                 message[0] = x
                 message[1] = y
+                # return rateを1 Hzに設定
+                try:
+                    await client.write_gatt_char(CHARACTERISTIC_UUID,message,response=True)
+                    await asyncio.sleep(delay)
+                    #print("connected")
+                except Exception:
+                    print("DISConnected now reconnect")
+                    break
+
+async def ble_bytes_reconnect(address,delay):
+    global x,y,r,g,b
+
+    while True:
+        async with BleakClient(
+            address
+        ) as client:
+            message_received = await client.read_gatt_char(CHARACTERISTIC_UUID)
+            print("message_received : {0}".format("".join(map(chr, message_received ))))
+            
+            while True:
+
+                #message = bytearray( b'\x30\x30')
+                message = bytearray(120 * 3)
+                for i in range(120):
+                    message[i*3] = r
+                    message[i*3+1] = g
+                    message[i*3+2] = b
+
+                    '''
+                    distance = abs( x - i)
+                    if distance<30:
+                        message[i] = 120- distance
+                    else:
+                        message[i] = 0
+                    '''
+                
                 # return rateを1 Hzに設定
                 try:
                     await client.write_gatt_char(CHARACTERISTIC_UUID,message,response=True)
@@ -107,7 +152,7 @@ async def ble_json(address,delay):
             await asyncio.sleep(delay)
 
 async def pygame_loop(delay):
-    global x,y
+    global x,y,r,g,b
     pygame.init() # 初期化
     screen = pygame.display.set_mode((120,120)) # ウィンドウサイズの指定
     pygame.display.set_caption("Pygame Test") # ウィンドウの上の方に出てくるアレの指定
@@ -119,12 +164,23 @@ async def pygame_loop(delay):
         text = font.render(f'{mouseX}, {mouseY}', True, (127,127,127))
         screen.fill((255, 255, 255))
         screen.blit(text, [0, 0])
-        pygame.draw.line(screen, (0,0,0), (mouseX,0), (mouseX,120), 1)
-        pygame.draw.line(screen, (0,0,0), (0,mouseY), (120,mouseY), 1)
-        pygame.display.flip()
+
         #pygame.display.update() # 画面更新
         x = mouseX
         y = mouseY
+
+        hue = mouseX / 120
+        lightness = mouseY / 120
+        saturation = 1.0
+
+        rTemp,gTemp,bTemp = colorsys.hls_to_rgb(hue,lightness,saturation)
+        r = round(255*rTemp)
+        g = round(255*gTemp)
+        b = round(255*bTemp)
+
+        pygame.draw.line(screen, (r,g,b), (mouseX,0), (mouseX,120), 1)
+        pygame.draw.line(screen, (r,g,b), (0,mouseY), (120,mouseY), 1)
+        pygame.display.flip()
 
         for event in pygame.event.get(): # 終了処理
             if event.type == QUIT:
@@ -137,8 +193,9 @@ async def main():
 
     task1 = asyncio.create_task(my_loop(1, 'hello'))
     task2 = asyncio.create_task(my_loop(2, 'world'))
+    task2 = asyncio.create_task(my_loop(2, 'world'))
     task3 = asyncio.create_task(pygame_loop(0.02))
-    task4 = asyncio.create_task(ble_byte_reconnect(address,0.02))
+    task4 = asyncio.create_task(ble_bytes_reconnect(address,0.02))
 
 
     print(f"started at {time.strftime('%X')}")
